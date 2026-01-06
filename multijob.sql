97d62b05df65a873d915b2532e2ed5efd3636d3b
RosewoodRidge <admin@rosewoodridge.xyz>
2026-01-07 05:25:13 +0800

    Updated SQL with more detail about how to modify the existing table or create a new one

--- multijob.sql
+++ multijob.sql
@@ -1,4 +1,8 @@
+-- Add the new column to the marshal_multi_jobs table
+ALTER TABLE marshal_multi_jobs
+ADD joblabel VARCHAR(255);
 
+-- Original SQL 
 CREATE TABLE marshal_multi_jobs (
 		`cid` INT(11) NOT NULL,
 		`job` varchar(255) NOT NULL,
@@ -9,3 +13,14 @@ CREATE TABLE marshal_multi_jobs (
 		PRIMARY KEY (`cid`, `job`)
 );
 
+-- Updated SQL with the new column 'joblabel'
+CREATE TABLE marshal_multi_jobs (
+    `cid` INT(11) NOT NULL,
+    `job` varchar(255) NOT NULL,
+    `joblabel` varchar(255) DEFAULT NULL,
+    `jobgrade` INT(11) NOT NULL,
+    `firstname` varchar(255) NOT NULL,
+    `lastname` varchar(255) NOT NULL,
+    `lastonline` varchar(255) NOT NULL,
+    PRIMARY KEY (`cid`, `job`)
+);
